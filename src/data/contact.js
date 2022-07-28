14d0a5cfc1ff001addf9f2450da9bc54c16f84b7
Calvin Kuo <32276838+Calvin2Kuo4U@users.noreply.github.com>
2022-07-28 18:24:37 -0400

    Changes to social icons in contact.

--- src/data/contact.js
+++ src/data/contact.js
@@ -2,8 +2,8 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
 import { faFacebookF } from '@fortawesome/free-brands-svg-icons/faFacebookF';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
-import { faAngellist } from '@fortawesome/free-brands-svg-icons/faAngellist';
-import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
+// import { faAngellist } from '@fortawesome/free-brands-svg-icons/faAngellist';
+// import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
 // See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
 // to add other icons.
@@ -29,6 +29,7 @@ const data = [
     label: 'LinkedIn',
     icon: faLinkedinIn,
   },
+  /*
   {
     link: 'https://angel.co/michael-d-angelo',
     label: 'Angel List',
@@ -39,6 +40,7 @@ const data = [
     label: 'Twitter',
     icon: faTwitter,
   },
+  */
   {
     link: 'mailto:calvinykuo@gmail.com',
     label: 'Email',
