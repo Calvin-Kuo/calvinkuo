10846eec24ca805977b3f5cbadcf071a97c7e64d
Calvin Kuo <32276838+Calvin2Kuo4U@users.noreply.github.com>
2023-04-13 20:58:14 -0400

    Updates to degrees.js code

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,13 +1,13 @@
 const degrees = [
   {
     school: 'Cornell University',
-    degree: 'M.Eng. Engineering Management',
+    degree: 'Master of Engineering (M.Eng.) Engineering Management',
     link: 'https://cornell.edu',
     year: 2024,
   },
   {
     school: 'Rutgers University - New Brunswick',
-    degree: 'B.S. Computer Science, Minor in Entrepreneurship',
+    degree: 'Bachelor of Science (B.S.) Computer Science, Minor in Entrepreneurship',
     link: 'https://rutgers.edu',
     year: 2020,
   },
